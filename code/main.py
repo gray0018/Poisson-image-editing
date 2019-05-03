72bf2c1d8de86cd4e80af2896cc08e74c23b0681
Yang Zhuoyu <yang.zhuoyu@ist.osaka-u.ac.jp>
2019-05-03 23:13:17 +0900

    fix the bug of wrong color, don't mod 255 when value out of 0~255

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 
@@ -8,57 +9,82 @@ def fix_source(source, mask, shape, offset):
     for i in range(mask.shape[0]):
         for j in range(mask.shape[1]):
             if mask[i][j]>127:
-                mydict[(i+offset[0],j+offset[1])] = counter
+                mydict[(i+offset[0], j+offset[1])] = counter
                 counter += 1
-    fixed_source = np.zeros(shape, dtype=int) #注意一定是uint8 不然最后imshow显示不出来#是int 不然后面*4会爆掉
-    fixed_source[offset[0]:source.shape[0]+offset[0], offset[1]:source.shape[1]+offset[1],:]=source
+    fixed_source = np.zeros(shape, dtype=int) #是int 不然后面*4会爆掉
+    # fixed_source = np.zeros(shape)
+    fixed_source[max(0, offset[0]):min(source.shape[0]+offset[0], shape[0]), max(0, offset[1]):min(source.shape[1]+offset[1],shape[1]),:]=source[max(0,-offset[0]):min(source.shape[0], shape[0]-offset[0]),max(0,-offset[1]):min(source.shape[1], shape[1]-offset[1]),:]
 
     return fixed_source, mydict
 
-offset = [[210, 10]]
-
-mask = cv2.imread("data/mask_01.jpg", 0)
-source = cv2.imread("data/source_01.jpg")
-target = cv2.imread("data/target_01.jpg")
-
-fixed_source, D = fix_source(source, mask, target.shape, offset[0]) #fixed source, same size with target
-
-A = np.zeros((len(D),len(D)), dtype=int)
-b = np.zeros((len(D),3), dtype=int)
-
-for k, v in D.items():
-    A[v][v] = 4
-    b[v] += 4*fixed_source[k[0]][k[1]] - fixed_source[k[0]+1][k[1]] - b[v] - fixed_source[k[0]-1][k[1]] - fixed_source[k[0]][k[1]+1] - fixed_source[k[0]][k[1]-1]
-
-    if (k[0]+1, k[1]) in D: # in D means this pixel is waiting to be calculated
-        A[v][D[(k[0]+1, k[1])]] = -1
-    else:
-        b[v] += target[k[0]+1][k[1]]
-
-    if (k[0]-1, k[1]) in D:
-        A[v][D[(k[0]-1, k[1])]] = -1
-    else:
-        b[v] += target[k[0]-1][k[1]]
-
-    if (k[0], k[1]+1) in D:
-        A[v][D[(k[0], k[1]+1)]] = -1
-    else:
-        b[v] += target[k[0]][k[1]+1]
-
-    if (k[0], k[1]-1) in D:
-        A[v][D[(k[0], k[1]-1)]]
-    else:
-        b[v] += target[k[0]][k[1]-1]
-
-x = np.linalg.lstsq(A, b, rcond=None)[0]
+offset = [[210, 10], [10, 28], [140, 80], [-40, 90], [60, 100], [-28, 88]]
 
 
-for k, v in D.items():
-    target[k[0]][k[1]][0] = np.uint8(round(x[v][0])%256)
-    target[k[0]][k[1]][1] = np.uint8(round(x[v][1])%256)
-    target[k[0]][k[1]][2] = np.uint8(round(x[v][2])%256)
+for pic_index in range(1, 5):
+    mask = cv2.imread("../data/mask_0{0}.jpg".format(pic_index), 0)
+    source = cv2.imread("../data/source_0{0}.jpg".format(pic_index))
+    target = cv2.imread("../data/target_0{0}.jpg".format(pic_index))
 
-# cv2.imshow("source01", fixed_source)
-cv2.imshow("target01", target)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    fixed_source, D = fix_source(source, mask, target.shape, offset[pic_index-1]) #fixed source, same size with target
+
+    A = np.zeros((len(D),len(D)), dtype=int)
+    b = np.zeros((len(D),3), dtype=int)
+
+    for k, v in D.items():
+        A[v][v] = 4
+        b[v] += 4*fixed_source[k[0]][k[1]] \
+            - fixed_source[k[0]+1][k[1]] \
+            - fixed_source[k[0]-1][k[1]] \
+            - fixed_source[k[0]][k[1]+1] \
+            - fixed_source[k[0]][k[1]-1]
+
+        if (k[0]+1, k[1]) in D: # in D means this pixel is waiting to be calculated
+            A[v][D[(k[0]+1, k[1])]] = -1
+        else:
+            b[v] += target[k[0]+1][k[1]]
+
+        if (k[0]-1, k[1]) in D:
+            A[v][D[(k[0]-1, k[1])]] = -1
+        else:
+            b[v] += target[k[0]-1][k[1]]
+
+        if (k[0], k[1]+1) in D:
+            A[v][D[(k[0], k[1]+1)]] = -1
+        else:
+            b[v] += target[k[0]][k[1]+1]
+
+        if (k[0], k[1]-1) in D:
+            A[v][D[(k[0], k[1]-1)]] = -1
+        else:
+            b[v] += target[k[0]][k[1]-1]
+
+    x = np.linalg.lstsq(A, b)[0]
+
+    for k, v in D.items():
+        if x[v][0]>255:
+            target[k[0]][k[1]][0] = np.uint8(255)
+        elif x[v][0]<0:
+            target[k[0]][k[1]][0] = np.uint8(0)
+        else:
+            target[k[0]][k[1]][0] = np.uint8(round(x[v][0]))
+
+        if x[v][1]>255:
+            target[k[0]][k[1]][1] = np.uint8(255)
+        elif x[v][1]<0:
+            target[k[0]][k[1]][1] = np.uint8(0)
+        else:
+            target[k[0]][k[1]][1] = np.uint8(round(x[v][1]))
+
+        if x[v][2]>255:
+            target[k[0]][k[1]][2] = np.uint8(255)
+        elif x[v][2]<0:
+            target[k[0]][k[1]][2] = np.uint8(0)
+        else:
+            target[k[0]][k[1]][2] = np.uint8(round(x[v][2]))
+
+
+        # target[k[0]][k[1]][0] = np.uint8(round(x[v][0])%256)
+        # target[k[0]][k[1]][1] = np.uint8(round(x[v][1])%256)
+        # target[k[0]][k[1]][2] = np.uint8(round(x[v][2])%256)
+
+    cv2.imwrite("result_0{0}.jpg".format(pic_index), target)
