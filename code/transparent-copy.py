befca494d858c824d89cce1a56e75aa917a9a6ae
Yang Zhuoyu <yang.zhuoyu@ist.osaka-u.ac.jp>
2019-05-14 15:23:38 +0900

    use csr_matrix and lsqr

--- code/transparent-copy.py
+++ code/transparent-copy.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
+from scipy import sparse
+from scipy.sparse.linalg import lsqr
 
 def fix_source(source, mask, shape, offset):
     mydict = {}
@@ -19,7 +21,7 @@ def fix_source(source, mask, shape, offset):
 offset = [[210, 10], [10, 28], [140, 80], [-40, 90], [60, 100], [-28, 88]]
 
 
-for pic_index in range(1, 5):
+for pic_index in range(4, 5):
     mask = cv2.imread("../data/mask_0{0}.jpg".format(pic_index), 0)
     source = cv2.imread("../data/source_0{0}.jpg".format(pic_index))
     target = cv2.imread("../data/target_0{0}.jpg".format(pic_index))
@@ -57,9 +59,18 @@ for pic_index in range(1, 5):
                 else:
                     b[v][i] += int(fixed_source[k[0]][k[1]][i])-int(fixed_source[k[0]][k[1]+j][i])
 
+    A = sparse.csr_matrix(A)
+    # b = sparse.csr_matrix(b)
 
-    x = np.linalg.lstsq(A, b)[0]
+    channel0 = lsqr(A, b[:,0])[0]
+    channel1 = lsqr(A, b[:,1])[0]
+    channel2 = lsqr(A, b[:,2])[0]
 
+    channel0 = channel0.reshape([channel0.shape[0], 1])
+    channel1 = channel1.reshape([channel1.shape[0], 1])
+    channel2 = channel2.reshape([channel2.shape[0], 1])
+
+    x = np.concatenate([channel0, channel1, channel2], axis=1)
     for k, v in D.items():
         for i in range(3):
             if x[v][i]>255:
